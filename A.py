557813e271e3e4ee714899c313c0e94efab98347
moon-start <login0516mp4@gmail.com>
2020-04-26 23:25:03 +0800

    All File

--- A.py
+++ A.py
@@ -2,9 +2,11 @@
 import os
 
 ##sh: 1: echo.QQ: not found
+
+##/home/travis/build/moon-start/BAT-QQ
 os.system("ls -al")
 os.system("pwd")
 
-print("A")
-print("ls -al")
-print("pws")
+#print("A")
+#print("ls -al")
+#print("pwd")
