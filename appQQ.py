b199ac2086726260634f2878b12f9a11f741627d
moon-start <login0516mp4@gmail.com>
2020-05-07 01:41:52 +0800

    All File

--- appQQ.py
+++ appQQ.py
@@ -1,28 +1,28 @@
-import json
-import os
-from selenium import webdriver
-def get_image_link(search_query):
-    img_urls = []
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN',None)
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
+# import json
+# import os
+# from selenium import webdriver
+# def get_image_link(search_query):
+#     img_urls = []
+#     chrome_options = webdriver.ChromeOptions()
+#     chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN',None)
+#     chrome_options.add_argument('--disable-gpu')
+#     chrome_options.add_argument('--no-sandbox')
 
-    ## 方案A
-    driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=os.getenv('CHROMEDRIVER_PATH',None))
+#     ## 方案A
+#     driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=os.getenv('CHROMEDRIVER_PATH',None))
    
-    # driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver')
+#     # driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver')
     
-    ## 方案B
-    # driver = webdriver.Chrome(executable_path='chromedriver')
+#     ## 方案B
+#     # driver = webdriver.Chrome(executable_path='chromedriver')
 
-    t = search_query[:-4]+'餐點價格'
-    url = 'https://www.google.com/search?q=' + t 
-    driver.get(url)
-    return url
-################################
-################################
-################################
+#     t = search_query[:-4]+'餐點價格'
+#     url = 'https://www.google.com/search?q=' + t 
+#     driver.get(url)
+#     return url
+# ################################
+# ################################
+# ################################
 
 
 
@@ -44,11 +44,12 @@ app = Flask(__name__)
 ## http://127.0.0.1:5000/  
 ## ,'POST'
 ## https://moon5566.herokuapp.com/
-@app.route('/', methods=['GET'])  
-def index():
-    return "GO :"+get_image_link("COCO")
+# @app.route('/', methods=['GET'])  
+# def index():
+#     return "GO :"+get_image_link("COCO")
 
-@app.route('/CMD/<string:SS>', methods=['GET'])  
+# @app.route('/CMD/<string:SS>', methods=['GET'])  
+@app.route('/<string:SS>', methods=['GET'])  
 def QQS(SS):
     SS =SS.replace('+','/')
     SSQ= os.popen(SS).read()   ## <str>
